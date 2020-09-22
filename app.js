d3078bcdbc6383fc3239319f153f19a87e9e489e
yenienRey <yenien@hotmail.com>
2020-09-22 18:52:46 -0300

    hola que tal

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const morgan = require("morgan"); //middleware application logger
+const nunjucks = require("nunjucks");
+
+const app = express(); // crea una instancia de una aplicación de express
+
+// Configurando Nunjucks
+app.set("view engine", "html"); // hace que res.render funcione con archivos html
+app.engine("html", nunjucks.render); // cuando le den archivos html a res.render, va a usar nunjucks
+nunjucks.configure("views"); // apunta a nunjucks al directorio correcto para los templates
+
+/* app.use(function (req, res, next) {
+  console.log(req.method);
+  console.log(req.url);
+  next();
+});
+
+app.use("/special", function (req, res, next) {
+  console.log("FUNCIONA");
+  next();
+}); */
+
+app.use(morgan("tiny"));
+
+let tweetsDeEjemplo = [
+  { id: 1, name: "juan", content: "este es un tweeettt de juan" },
+  { id: 2, name: "carlos", content: "este es un tweeettt de carlos" },
+  { id: 3, name: "pepe", content: "este es un tweeettt de pepe" },
+];
+
+app.get("/", function (req, res) {
+  res.render("index", { tweets: tweetsDeEjemplo });
+});
+
+app.use(express.static('./public'))
+
+
+app.listen(3000, function () {
+  console.log("Estas escuhando en el puerto 3000");
+});
