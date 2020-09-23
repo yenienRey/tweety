b92b30f505c407f2076e1e3c6f179ed7205bc6bc
yenienRey <yenien@hotmail.com>
2020-09-23 12:56:16 -0300

    pusheando

---
+++ routes/index.js
@@ -0,0 +1,36 @@
+const tweetBank = require('../tweetBank');
+const express = require('express');
+
+const router = express.Router();
+// Se puede usar solo una linea: const router = require('express').Router();
+
+router.get('/', function (req, res) {
+  let tweets = tweetBank.list();
+  res.render( 'index', { tweets: tweets, showForm: true } );
+});
+
+router.get('/users/:name', function(req, res) {
+    var name = req.params.name;
+    var list = tweetBank.find( { name: name } );
+    res.render( 'index', { tweets: list, showForm: true, tweetero: name} );
+});
+
+router.get('/tweets/:id', function(req, res) {
+    var id = Number(req.params.id);
+    var list = tweetBank.find( { id: id } );
+    res.render( 'index', { tweets: list } );
+});
+
+router.post('/tweets', function(req, res) {
+    var name = req.body.name;
+    var text = req.body.text;
+    tweetBank.add(name, text);
+    res.redirect('/');
+});
+
+module.exports = function (io) {
+    // ...
+    // definiciones de rutas, etc.
+    // ...
+    return router;
+  };
